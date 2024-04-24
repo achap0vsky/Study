4326ea1066734325dbbdfe967e47557531ba82e4
achap0vsky <achapovskii@gmail.com>
2024-04-24 19:04:39 +0200

    Lesson 8

--- m2_lesson4_step8.py
+++ m2_lesson4_step8.py
@@ -2,18 +2,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+import math
+import time
 
-browser = webdriver.Chrome()
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+try:
+  link = "http://suninjuly.github.io/explicit_wait2.html"
+  browser = webdriver.Chrome()
+  browser.get(link)
+  price = WebDriverWait(browser, 13).until(
+            EC.text_to_be_present_in_element((By.ID, "price"),"100")
+        )
+  button = browser.find_element(By.ID,"book")
+  button.click()
+  x_element = browser.find_element(By.ID,"input_value")
+  x = x_element.text
+  answer = calc(x)
+  input1 = browser.find_element(By.ID,"answer")
+  input1.send_keys(answer)
+  submit = browser.find_element(By.ID,"solve")
+  submit.click()
+finally:
+    time.sleep(5)
+    browser.quit()
 
-browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-button = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.ID, "verify"))
-    )
-button.click()
-message = browser.find_element(By.ID, "verify_message")
-
-assert "successful" in message.text
 
 
